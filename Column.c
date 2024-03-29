7f609d09c4bb80480568aa8b6098fc27f1d22143
noahj <noah.jeandeau@gmail.com>
2024-03-29 13:37:05 +0100

    init

--- Column.c
+++ Column.c
@@ -1,1 +1,32 @@
 #include "Column.h"
+#include <stdio.h>
+
+
+void test(){
+
+    printf("I guess the test is working");
+
+}
+
+COLUMN *create_column(ENUM_TYPE type , char* title){
+
+    //creation of the column
+    COLUMN col1;
+
+    //attribution of the parameters of the newly created column
+    col1.title = title ;
+
+    col1.size = 0;
+
+    col1.max_size = 0;
+
+    col1.column_type = type ;
+
+    col1.data = NULL ;
+
+    col1.index = NULL ;
+
+    //returning the adress of the newly created column
+    return &col1;
+
+}
--- Column.h
+++ Column.h
@@ -1,3 +1,4 @@
+
 #ifndef _CDATAFRAMES_COLUMN_H
 #define _CDATAFRAMES_COLUMN_H
 
@@ -8,16 +9,54 @@
 * @return : Pointer to created column
 */
 
+enum enum_type{
+
+    NULLVAL = 1, UINT, INT, CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
+
+};
+typedef enum enum_type ENUM_TYPE;
+
+
+
+union column_type{
+    unsigned int uint_value;
+    signed int int_value;
+    char char_value;
+    float float_value ;
+    double double_value ;
+    char* string_value ;
+    void* struct_value ;
+
+
+};
+typedef union column_type COL_TYPE ;
+
+
+
 typedef struct{
 
-    int PhysicalSize ;
+        char *title;
 
-    char* column_name ;
+        unsigned int size; //logical size
 
-    void* column_array ;
+        unsigned int max_size; //physical size
+
+        ENUM_TYPE column_type;
+
+        COL_TYPE **data; // array of pointers to stored data
+
+        unsigned long long int *index; // array of integers
+
 
 }COLUMN;
 
-COLUMN *create_column(char *title);
+
+
+COLUMN *create_column(ENUM_TYPE type , char *title);
+
+
+
+void test();
+
 
 #endif
