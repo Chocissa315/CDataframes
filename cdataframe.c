fd9c00e465379925b9a1ae554addac61f0704bf1
Chocissa315 <150431999+Chocissa315@users.noreply.github.com>
2024-05-02 09:20:25 +0200

    Add of the functions in list.C and create cdataframe function to revise in case of potential errors

--- cdataframe.c
+++ cdataframe.c
@@ -2,37 +2,40 @@
 #include "list.h"
 #include <stdio.h>
 
-/
+
 
 CDATAFRAME *create_cdataframe(ENUM_TYPE *cdftype, int size) {
-    ENUM_TYPE type;
-    char title;
+    ENUM_TYPE type = *cdftype;
+    char title[100];
     void *dat;
-    lnode ptemp;
+    lnode *ptemp;
     list *liste;
 
-
     printf("Enter the column's name: ");
-    scanf("%s", &title);
+    scanf("%s", title);
     liste = lst_create_list();
 
-    for(int i=0; i < size; i++){
-        dat = create_column(type, &title);
-        ptemp = *lst_create_lnode(dat);
-        if(i == 0) {
-            lst_insert_head(liste, &ptemp);
+    for (int i = 0; i < size; i++) {
+        dat = create_column(type, title);
+        ptemp = lst_create_lnode(dat);
+        if (i == 0) {
+            lst_insert_head(liste, ptemp);
+        } else {
+            lst_insert_tail(liste, ptemp);
         }
-        else{
-            lst_insert_tail(liste, &ptemp);
-        }
-
     }
 
     return liste;
-
 }
 
 
+
+void delete_cdataframe(CDATAFRAME **cdf){
+
+    /* What I can do is go through the Linked list and delete one by one the nodes which represents the cols
+     * in theory it can work Avec le go to the next it can work et on vérifie que le node n'est pas null*/
+
+}
 
 int get_cdataframe_cols_size(CDATAFRAME *cdf){
 
